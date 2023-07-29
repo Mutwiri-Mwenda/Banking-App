f7fda1cb40330002aa7e0785902d480b3d7bab25
Mutwiri <65445617+Mutwiri-Mwenda@users.noreply.github.com>
2023-07-29 21:38:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-import Navbar from "./Navbar";
-import Billing from "./Billing";
-import CardDeal from "./CardDeal";
-import Business from "./Business";
-import Clients from "./Clients";
-import CTA from "./CTA";
-import Stats from "./Stats";
-import Footer from "./Footer";
-import Testimonials from "./Testimonials";
-import Hero from "./Hero";
-
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
 
 export {
-  Navbar,
-  Billing,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-  Testimonials,
-  Hero,
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
 };
